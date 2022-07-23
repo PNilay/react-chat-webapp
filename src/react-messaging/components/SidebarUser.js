d7cffdd249ba7dab6b5302d230b5accd63401264
Pnilay <pnilay926@gmail.com>
2022-07-23 14:33:31 -0400

    added functionality to display last message under chat users

--- src/react-messaging/components/SidebarUser.js
+++ src/react-messaging/components/SidebarUser.js
@@ -3,57 +3,61 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { auth, db } from "../../firebase";
 import "./SidebarUser.css";
+import {
+  addDoc,
+  collection,
+  setDoc,
+  doc,
+  Timestamp,
+  FieldValue,
+  onSnapshot,
+  query,
+  where,
+  getDocs,
+  orderBy,
+} from "firebase/firestore";
+import { useStateValue } from "../reactContext/StateProvider";
 
-function SidebarUser({ id, user, addNewChat }) {
+function SidebarUser({ id, userl, addNewChat }) {
   const [seed, setSeed] = useState("");
 
   const [messages, setMessages] = useState("");
+  const [{ user }, dispatch] = useStateValue(); //information related to current user (sender)
 
-  // useEffect(() => {
-  //   if (id) {
-  //     db.collection("rooms")
-  //       .doc(id)
-  //       .collection("messages")
-  //       .orderBy("timestamp", "desc")
-  //       .onSnapshot((snapshot) =>
-  //         setMessages(snapshot.docs.map((doc) => doc.data()))
-  //       );
-  //   }
-  // },[id]);
-  // useEffect(() => {
-  //   setSeed(Math.floor(Math.random() * 5000));
-  // }, []);
+  useEffect(() => {
+    if (id) {
+      const combined_id =
+        user.uid > id ? `${user.uid + id}` : `${id + user.uid}`;
+
+      const messgaesRef = collection(db, "messages", combined_id, "chat");
+
+      const q2 = query(messgaesRef, orderBy("timestamp", "asc"));
+
+      onSnapshot(q2, (querySnapshot) => {
+        let msgs = [];
+        querySnapshot.forEach((doc) => {
+          msgs.push(doc.data());
+        });
+        setMessages(msgs);
+      });
+    }
+  }, [id]);
 
   const createChat = () => {
     const roomName = prompt("Please enter name for chat");
-
-    // if (roomName) {
-    //   db.collection("rooms").add({
-    //     name: roomName,
-    //   });
-    // }
   };
 
   return !addNewChat ? (
     <Link to={`/chat/${id}`}>
-    <div className="sidebar_user">
-      <Avatar src={`https://avatars.dicebear.com/api/avataaars/${id}.svg`} />
-      <div className="sidevaruser__info">
-        <h2>{user.name}</h2>
-        <p>msg</p>
+      <div className="sidebar_user">
+        <Avatar src={`https://avatars.dicebear.com/api/avataaars/${id}.svg`} />
+        <div className="sidevaruser__info">
+          <h2>{userl.name}</h2>
+          <p>{messages[0]?.message}</p>
+        </div>
       </div>
-    </div>
     </Link>
   ) : (
-    // <Link to={`/chat/${id}`}>
-    //   <div className="sidebar_user">
-    //     <Avatar src={`https://avatars.dicebear.com/api/avataaars/${id}.svg`} />
-    //     <div className="sidevaruser__info">
-    //       <h2>{name}</h2>
-    //       <p>{messages[0]?.message}</p>
-    //     </div>
-    //   </div>
-    // </Link>
     <div onClick={createChat} className="sidebar_user">
       <h2>Add new Chat</h2>
     </div>

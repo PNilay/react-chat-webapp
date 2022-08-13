681b157dabca1d4901551f486b49d686807ec1cf
Pnilay <pnilay926@gmail.com>
2022-08-12 23:37:30 -0400

    components to display closed and current polls

---
+++ src/react-messaging/components/polls/ClosedPolls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ClosedPolls() {
+  return (
+    <div>
+      Closed Polls
+    </div>
+  )
+}
+
+export default ClosedPolls;
---
+++ src/react-messaging/components/polls/CurrentPolls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CurrentPolls() {
+  return (
+    <div>
+      Current Polls
+    </div>
+  )
+}
+
+export default CurrentPolls;

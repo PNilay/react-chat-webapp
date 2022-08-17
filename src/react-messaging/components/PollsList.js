6a279945d7a0852c383ed504a72a69e95d73e877
Pnilay <pnilay926@gmail.com>
2022-08-16 21:54:09 -0400

    added functionality to display chatroom users/ group name

--- src/react-messaging/components/PollsList.js
+++ src/react-messaging/components/PollsList.js
@@ -18,6 +18,19 @@ import Box from "@mui/material/Box";
 import CurrentPolls from "./polls/CurrentPolls";
 import ClosedPolls from "./polls/ClosedPolls";
 import CreatePolls from "./polls/CreatePolls";
+import { useStateValue } from "../reactContext/StateProvider";
+import { auth, db } from "../../firebase";
+import { updateDoc, doc, onSnapshot, QuerySnapshot } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  Timestamp,
+  addDoc,
+  setDoc,
+  arrayUnion,
+  arrayRemove,
+} from "firebase/firestore";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -41,6 +54,8 @@ function TabPanel(props) {
 
 function PollsList() {
   const [value, setValue] = React.useState(0);
+  const [{ user }, dispatch] = useStateValue(); //information related to current user (sender)
+  const [name, setName] = useState("");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -50,16 +65,55 @@ function PollsList() {
 
   const { userId } = useParams(); //userId of charroom/group
 
+  useEffect(() => {
+    if (userId.includes(user.uid)) {
+      console.log("Singal Chat ");
+      // Single chat
+      const t = userId.replace(user.uid, "");
+      const usersRef = collection(db, "users");
+      const q = query(usersRef, where("uid", "in", [user.uid, t]));
+
+      let chatusers = "";
+
+      const name = [];
+      onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach(async (doc) => {
+          name.push(doc.data());
+        });
+
+        const name2 = " " + name[0].name + " & " + name[1].name;
+
+        setName(name2);
+      });
+    } else {
+      // Group Chat
+      const usersRef = collection(db, "groups");
+      const q = query(usersRef, where("uid", "==", userId));
+
+      onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          setName(doc.data().groupName);
+        });
+      });
+    }
+  }, []);
+
   return (
     <div className="pollsList">
       <div className="pollsList__header">
         <div className="pollsList__profile">
           <Avatar
-            src={`https://avatars.dicebear.com/api/bottts/${userId}.svg`}
+            src={`https://avatars.dicebear.com/api/bottts/${userId.replace(
+              user.uid,
+              ""
+            )}.svg`}
           />
         </div>
         <div className="pollsList__headerInfo">
           <div className="pollsList__title">POLLS</div>
+          <div className="attractionList__title">
+            <b>{name}</b>
+          </div>
         </div>
 
         <div className="pollsList__headerRight">
